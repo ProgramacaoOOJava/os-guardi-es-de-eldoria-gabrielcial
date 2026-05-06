64486601bc54b7b47e0c2c772002807c17bd58a3
Gabriel | Cavantech <156324082+gabrielcial@users.noreply.github.com>
2026-05-06 23:22:24 +0000

    Adicionei algumas strings

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("=== BEM-VINDOS AO REINO DE ELDORIA ===");
         Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
         Personagem heroi2 = new Personagem("Arthemis", "Arqueira", 5, 80, 12.5);
+        System.out.println("--- Herói 1 ---");
         heroi1.exibirStatus();
+        System.out.println("--- Herói 2 ---");
         heroi2.exibirStatus();
     }
 }
