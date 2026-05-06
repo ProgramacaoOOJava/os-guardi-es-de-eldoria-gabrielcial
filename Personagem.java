cb541c313ba8b5140b6bb3998d4462d695b01b28
Gabriel | Cavantech <156324082+gabrielcial@users.noreply.github.com>
2026-05-06 23:11:29 +0000

    Adicionado complemento ao codigo

--- Personagem.java
+++ Personagem.java
@@ -7,18 +7,18 @@ public class Personagem {
 
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     }
 
     public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+        System.out.println("Nome: " + nome);
+        System.out.println("Classe: " + classe);
+        System.out.println("Nível: " + nivel);
+        System.out.println("Pontos de Vida: " + pontosDeVida);
+        System.out.println("Poder Base: " + poderBase);
     }
 }
 
